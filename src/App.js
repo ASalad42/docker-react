971a3d6b613401410cd49af9d4fd4af4bffa7427
ayan <ayanie@live.co.uk>
2023-02-01 14:43:52 +0000

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
-          Edit <code>src/App.js</code> and save to reload made this change on Localhost.
-=======
-          Making change on a feature branch
->>>>>>> a50e21173dcaa4ffad6b1dbcbc774a5fb390f0eb
+          Edit <code>src/App.js</code> and save to reload. changing on local host
         </p>
         <a
           className="App-link"
@@ -27,3 +23,5 @@ function App() {
 }
 
 export default App;
+
+
