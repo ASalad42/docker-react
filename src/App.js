d2c2313a6b3c26880a60117352c525f7ff59fbe6
ayan <ayanie@live.co.uk>
2023-02-01 15:15:31 +0000

    change on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. changing on local host in feature branch
+          Edit <code>src/App.js</code> and save to reload. changing on local host in feature branch 2
         </p>
         <a
           className="App-link"
